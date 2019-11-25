466b88da194e4f850c0d98934a2ce6602eee12b7
shaunhossain <shaunhossain95@gmail.com>
2019-11-25 23:52:47 +0600

    2nd test

--- app/src/main/java/com/shaunhossain/networklibrary/MainActivity.java
+++ app/src/main/java/com/shaunhossain/networklibrary/MainActivity.java
@@ -7,13 +7,16 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        for (int i ; i<=5;i++){
 
-            //this is my 2nd commit.
-        }
+    }
+    private void test(){
+
+        //bal cal
     }
 }
