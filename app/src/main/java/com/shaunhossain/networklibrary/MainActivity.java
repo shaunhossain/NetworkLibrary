961df15754890a9aa2d5081efd5bc5061dfba0af
Shaun Hossain <shaunhossain95@gmail.com>
2019-11-26 23:34:15 +0600

    Update MainActivity.java

--- app/src/main/java/com/shaunhossain/networklibrary/MainActivity.java
+++ app/src/main/java/com/shaunhossain/networklibrary/MainActivity.java
@@ -15,13 +15,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
-    private void test(){
-
-        //bal cal cole na
-    }
-
-    private void test_bal() {
-
-
-    }
+   
 }
